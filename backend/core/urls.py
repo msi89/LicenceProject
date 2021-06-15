9582d518cb629f8756fb63826fc3eb8c70201def
Ckam <ckam225@gmail.com>
2021-06-15 11:48:56 -0700

    clean

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,37 +1,16 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from core import settings
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic.base import TemplateView
-from django.contrib.auth.decorators import login_required
 from django.conf.urls.static import static
-from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.signin),
-    path('logout/', views.signout, name='logout'),
     path('api/', include('drives.urls')),
     re_path(
-        '(^(?!(api|admin|logout|login|media)).*$)',
-        login_required(TemplateView.as_view(template_name="index.html"),
-                       login_url='login/')),
+        '(^(?!(api|admin|media)).*$)',
+        TemplateView.as_view(template_name="index.html")),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
